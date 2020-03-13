ada5ba4d40e01216d67bce18a1cb8e9cff3d5602
midni8coder <62158642+midni8coder@users.noreply.github.com>
2020-03-14 03:12:56 +0530

    Add files via upload

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,55 @@
+﻿using isMART.API.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace isMART.API.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Title = "Home Page";
+
+            return View();
+        }
+        public async Task<ActionResult> GetProductsFromAPI(int productID=0)
+        {
+            HttpClient client = new HttpClient();
+            client.BaseAddress = new Uri("https://localhost:44355/");
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(
+                new MediaTypeWithQualityHeaderValue("application/json"));
+            Product product = null;
+            List<Product> products = new List<Product>();
+            if (productID > 0)
+            {
+                HttpResponseMessage response = await client.GetAsync("api/Product/GetProductDetail?productID=1");
+                if (response.IsSuccessStatusCode)
+                {
+                    product = await response.Content.ReadAsAsync<Product>();
+                    products.Add(product);
+                }
+
+                //return Json(product, JsonRequestBehavior
+                //    .AllowGet);
+            }
+            else
+            {
+                HttpResponseMessage response = await client.GetAsync("api/Product/GetAllProducts");
+                if (response.IsSuccessStatusCode)
+                {
+                    products = await response.Content.ReadAsAsync<List<Product>>();
+                }
+                //return Json(product, JsonRequestBehavior
+                //    .AllowGet);
+            }
+            return View(products);
+        }
+    }
+}
---
+++ Controllers/ProductController.cs
@@ -0,0 +1,84 @@
+﻿using isMART.API.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace isMART.API.Controllers
+{
+    public class ProductController : ApiController
+    {
+        // GET api/<controller>
+        public static List<Product> products = new List<Product>();
+        public void SetProductDetails()
+        {
+            products.Add(new Product()
+            {
+                Name = "AXE Refresh Deo",
+                AvailableQuantity = 10,
+                Price = 190,
+                ProductCategory = "Deo&Perfumes",
+                ProductCode = "AXERDM12",
+                ProductID = 1,
+                SellerID = 1,
+                TotalQuantity = 15
+            });
+
+            products.Add(new Product()
+            {
+                Name = "Killer Jeans",
+                AvailableQuantity = 25,
+                Price = 3499,
+                ProductCategory = "MensWear",
+                ProductCode = "KillerXJ900",
+                ProductID = 2,
+                SellerID = 1,
+                TotalQuantity = 53
+            });
+
+            products.Add(new Product()
+            {
+                Name = "Rebook Shoes",
+                AvailableQuantity = 23,
+                Price = 3250,
+                ProductCategory = "Shoes",
+                ProductCode = "RBKM093",
+                ProductID = 3,
+                SellerID = 1,
+                TotalQuantity = 42
+            });
+        }
+
+        public IEnumerable<Product> GetAllProducts()
+        {
+            if (products.Count == 0)
+                SetProductDetails();
+            return products;
+        }
+
+        // GET api/<controller>/5
+        public Product GetProductDetail(int productID)
+        {
+            if (products.Count == 0)
+                SetProductDetails();
+            return products.Where(x => x.ProductID == productID).FirstOrDefault();
+        }
+
+        // POST api/<controller>
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/<controller>/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/<controller>/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
